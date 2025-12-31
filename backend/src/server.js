6204a403605b606b9d7092f35caf9cfbb52c32e7
Ashish Gautam <gautamashish484@gmail.com>
2026-01-01 00:18:25 +0530

    remove frontend serving from backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,13 +14,5 @@ app.get('/books', (req,res) => {
     res.status(200).json({ msg : "This is the books endpoint"});   
 });
 
-//make ou app ready for deployment
-if(ENV.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-    app.get("/{*any}", (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-    })
-}
-
-app.listen(ENV.PORT, () => console.log("server is running on port 3000"));
+app.listen(ENV.PORT, () => {console.log(`Server running on port ${ENV.PORT}`)});
